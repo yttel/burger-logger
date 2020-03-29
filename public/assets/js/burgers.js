4c76a6a3ef2491fd27dfd3066d97464dc536c51e
BytteL <lettybedard@gmail.com>
2020-03-29 19:10:46 -0400

    first pass

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,49 @@
+$(function() {
+  $(".eat-burger").on("click", function(event) {
+    const id = $(this).data("id");
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: id
+    }).then(
+      function() {
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    const newBurger = {
+      name: $("#brgr").val().trim(),
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        location.reload();
+      }
+    );
+  });
+
+  // $(".delete-cat").on("click", function(event) {
+  //   var id = $(this).data("id");
+
+  //   // Send the DELETE request.
+  //   $.ajax("/api/cats/" + id, {
+  //     type: "DELETE"
+  //   }).then(
+  //     function() {
+  //       console.log("deleted cat", id);
+  //       // Reload the page to get the updated list
+  //       location.reload();
+  //     }
+  //   );
+  // });
+});
